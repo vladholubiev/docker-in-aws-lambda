3ff106393a5662b4c6ee0fb62f888f5e913f4d4b
Vlad Holubiev <golubev.vld@gmail.com>
2017-06-04 15:12:46 +0300

    Add missing --nosysdirs arg

--- ubuntu-demo.sh
+++ ubuntu-demo.sh
@@ -18,4 +18,4 @@ python udocker create --name=ubuntu ubuntu:17.04
 python udocker setup --execmode=F1 ubuntu
 
 # Run it!
-python udocker run ubuntu cat /etc/os-release
+python udocker run --nosysdirs ubuntu cat /etc/os-release
